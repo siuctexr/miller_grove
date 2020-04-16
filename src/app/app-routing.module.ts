448c86a16d520031a6bfdc30997a66a5c2e9bcef
Bijay Raj Paudel <bijayraj.paudel@siu.edu>
2020-04-16 02:03:32 -0500

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeHeaderComponent } from './home-header/home-header.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeHeaderComponent },
+  { path: 'home', component: HomeHeaderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { NavBarTopComponent } from './nav-bar-top/nav-bar-top.component';
+import { HomeHeaderComponent } from './home-header/home-header.component';
+
+import { MaterialModule } from './material/material.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    NavBarTopComponent,
+    HomeHeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
